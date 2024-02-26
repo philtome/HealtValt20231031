35f0ef84fa7b30a57f565a597f70286202a1b05f
m1mac <phil@tome.net>
2024-02-26 05:19:13 -0700

    after updating M1

--- app/Resources/health valut users 20240226.sql
+++ app/Resources/health valut users 20240226.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Oct 27, 2023 at 04:21 PM
+-- Generation Time: Feb 26, 2024 at 12:17 PM
 -- Server version: 5.7.39
 -- PHP Version: 8.2.0
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `skycliff`
+-- Database: `health_history`
 --
 
 -- --------------------------------------------------------
@@ -31,6 +31,9 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `uid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `pwd` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `lastName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `firstName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pwdHint` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `admin` tinyint(1) DEFAULT NULL,
   `active` tinyint(1) DEFAULT NULL,
@@ -41,8 +44,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `uid`, `pwd`, `email`, `admin`, `active`, `lastSignon`) VALUES
-(1, 'sysadmin', '$2y$10$WtXLLxTK869dsWtT6BH50uWCgykKF4raopIdNBRCP2IftX.6/bMN.', 'phil@tome.net', 0, 0, NULL);
+INSERT INTO `users` (`id`, `uid`, `pwd`, `lastName`, `firstName`, `pwdHint`, `email`, `admin`, `active`, `lastSignon`) VALUES
+(1, 'sysadmin', '$2y$10$WtXLLxTK869dsWtT6BH50uWCgykKF4raopIdNBRCP2IftX.6/bMN.', NULL, NULL, NULL, 'phil@tome.net', 0, 0, NULL),
+(2, 'ptome', '$2y$10$2PIidJ.sJk56Np.nw..zduDChmjYBi0yD0M7BDblzR/0f6GfPyUNe', '', '', 'freddy', 'freddy reddy again', 0, 0, NULL),
+(3, 'lexitome', '$2y$10$T9iSV9gqI2/OKWM/fF6aJ.YlA.U38xQaR15dHQngMYp5o1qhDDy2e', 'tome', 'lexi', 'Lusah005', 'lexi@tome.net', 1, 1, NULL);
 
 --
 -- Indexes for dumped tables
@@ -62,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
